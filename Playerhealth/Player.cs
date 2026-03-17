e74fc99b901392386ab3d9e6064b430899afc184
Isaac Stumm <isaac.stumm@students.snow.edu>
2026-03-17 16:02:16 -0600

    Add project files.

---
+++ Playerhealth/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Main;
+namespace Playerhealth
+{
+    internal class Player
+    {
+        public int Health { get; private set; }
+        public Player() 
+        {
+            Health = 100;
+        }
+        public void TakeDamage(int amount)
+        {
+            if ((Health - amount)<= 0)
+            {
+                Health = 0;
+                Console.WriteLine("Player defeated");
+                
+            }
+            else
+            {
+            Health -= amount;
+                Console.WriteLine($"Health: {Health}");
+            }
+        }
+        public void Heal(int amount)
+        {
+            if ((Health + amount) >= 100)
+            {
+                Health = 100;
+                Console.WriteLine($"Health: {Health}");
+            }
+            else
+            {
+                Health += amount;
+                Console.WriteLine($"Health: {Health}");
+            }
+        }
+    }
+}
---
+++ Playerhealth/Program.cs
@@ -0,0 +1,47 @@
+﻿using Playerhealth;
+namespace Main
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Player player = new Player();
+            bool codeRunning = true;
+            while (codeRunning)
+            {
+                Console.WriteLine($"Player has {player.Health} health, what do you do? (Type word)\n" +
+                    "1.Attack\n" +
+                    "2.Heal\n" +
+                    "3.End");
+                string response = Console.ReadLine().Trim().ToLower();
+                switch (response)
+                {
+                    case "attack":
+                        Console.WriteLine("How much?");
+                        int damage = int.Parse(Console.ReadLine());
+                        player.TakeDamage(damage);
+                        if(player.Health <=0)
+                        {
+                            codeRunning = false;
+                        }
+                        break;
+                    case "heal":
+                        Console.WriteLine("How much?");
+                        int replenish = int.Parse(Console.ReadLine());
+                        player.TakeDamage(replenish);
+                        break;
+                    case "end":
+                        codeRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Not a valid operation");
+                        break;
+
+                }
+
+            }
+        }
+    }
+
+
+}
